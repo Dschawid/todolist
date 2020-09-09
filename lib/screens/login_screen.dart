65be4cc84f32f76f1929b8bdf404ec3b2939d03b
Deniz <altun.deniz@metu.edu.tr>
2020-09-09 14:45:38 +0300

    login_screen.dart

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,8 @@
+import './screens/login_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Container();)
+  }
+}
